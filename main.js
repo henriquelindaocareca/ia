9a6d10aad095aa6b33cb6207a21940f707545bb9
henrique <102448580+henriquelindaocareca@users.noreply.github.com>
2024-07-02 12:30:06 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,30 @@
+const caixaPrincipal = document.querySelector(".caixa-principal");
+const caixaPerguntas = document.querySelector(".caixa-perguntas");
+const caixaAlternativas = document.querySelector(".caixa-alternativas");
+const caixaResultado = document.querySelector(".caixa-resultado");
+const textoResultado = document.querySelector(".texto-resultado");
+
+const perguntas = [
+    {
+        enunciado = "É responsável dar comida típica para animais em adoção?"
+        Alternativas: [
+            {
+                texto: "Sim",
+                afirmação: "afirmação"
+            },
+            {
+                texto: "Não",
+                afirmação: "afirmação"
+            }
+        ]
+    },
+    {
+        enunciado = "Como o acompanhamento veterinário pode influenciar a saúde dos animais em desfiles culturais e como a coleta seletiva ajuda para a sustentabilidade desses eventos?"
+        Alternativas: [
+            {
+                texto: "Pode ajudar"
+                afirmação: "afirmação"
+            }
+        ]
+    }
+]
