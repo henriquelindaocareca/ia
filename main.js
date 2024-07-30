53c70768b5ab5e8bb89ee25c9a47ca0b1ba1cedc
henrique <102448580+henriquelindaocareca@users.noreply.github.com>
2024-07-30 12:32:12 -0300

    Update main.js

--- main.js
+++ main.js
@@ -6,25 +6,36 @@ const textoResultado = document.querySelector(".texto-resultado");
 
 const perguntas = [
     {
-        enunciado = "É responsável dar comida típica para animais em adoção?"
+        enunciado = "É responsável dar comida típica para animais em adoção?",
         Alternativas: [
             {
                 texto: "Sim",
-                afirmação: "afirmação"
+                afirmação: "Faz com que o animal não passe fome."
             },
             {
                 texto: "Não",
-                afirmação: "afirmação"
+                afirmação: "Faz mal ao animal e pode levar o mesmo à óbito."
             }
         ]
     },
     {
-        enunciado = "Como o acompanhamento veterinário pode influenciar a saúde dos animais em desfiles culturais e como a coleta seletiva ajuda para a sustentabilidade desses eventos?"
+        enunciado = "Como o acompanhamento veterinário pode influenciar a saúde dos animais em desfiles culturais e como a coleta seletiva ajuda para a sustentabilidade desses eventos?",
         Alternativas: [
             {
-                texto: "Pode ajudar"
+                texto: "Pode ajudar",
                 afirmação: "afirmação"
             }
         ]
     }
 ]
+
+let atual = 0
+let perguntaAtual;
+let historiaFinal = "";
+
+function mostraPergunta() {
+    if (atual >= perguntas.length) {
+        mostraResultado();
+    }
+}
+
